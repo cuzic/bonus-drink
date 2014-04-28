9dfb8a834673215bc1061f7d0b0db26439f16d4c
cuzic <cuzic@cuzic.com>
2014-04-29 08:28:54 +0900

    cuzic's answer with recursive call

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,18 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    self.calculate(amount, amount)
   end
+
+  def self.calculate(count, empty_bottles)
+    return count if empty_bottles < 3
+
+    div, mod = empty_bottles.divmod 3
+
+    return calculate(count + div, mod + div)
+  end
+end
+
+if $0 == __FILE__ then
+  count = BonusDrink.total_count_for ARGV.shift.to_i
+  puts count
 end
